eaebbf25f7a01424f7dc14a82082817d554053c8
Jack Cable <jackhcable@gmail.com>
2019-06-11 18:26:02 -0700

    Update LSTM/supervised algs

--- run_all.py
+++ run_all.py
@@ -1,48 +1,43 @@
-# Most of this is from https://www.analyticsvidhya.com/blog/2018/04/a-comprehensive-guide-to-understand-and-implement-text-classification-in-python/
+# Outline from https://www.analyticsvidhya.com/blog/2018/04/a-comprehensive-guide-to-understand-and-implement-text-classification-in-python/
 
 from sklearn import model_selection, preprocessing, linear_model, naive_bayes, metrics, svm
+from sklearn.model_selection import GridSearchCV
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.feature_extraction import text as text2
 from sklearn import decomposition, ensemble
 
 import pandas, numpy as np, textblob, string
-from keras.preprocessing import text, sequence
-from keras import layers, models, optimizers
 from sklearn.utils import shuffle
 import operator as op
 
 import warnings
 
 if __name__ == "__main__":
-	nb = True
-	lr = True
-	svm_ = True
-	rf = True
-	shallow_nn = False
-	deep_nn = False
+	nb = False
+	lr = False
+	svm_ = False
+	rf = False
 else:	
 	nb = False
 	lr = False
 	svm_ = False
 	rf = False
-	shallow_nn = False
-	deep_nn = False
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-def train_model(classifier, feature_vector_train, label, feature_vector_valid, is_neural_net=False, should_do_common=False):
-    # fit the training dataset on the classifier
-    classifier.fit(feature_vector_train, label)
-    
-    train_preds = classifier.predict(feature_vector_train)
-    valid_preds = classifier.predict(feature_vector_valid)
+def train_model(classifier, feature_vector_train, label, feature_vector_test, is_neural_net=False, should_do_common=False):
+	# fit the training dataset on the classifier
+	classifier.fit(feature_vector_train, label)
+	
+	train_preds = classifier.predict(feature_vector_train)
+	test_preds = classifier.predict(feature_vector_test)
 
-    stop_words = text2.ENGLISH_STOP_WORDS.union(["http", "https", "amp", "amb"])
-    
-    if is_neural_net:
-        valid_preds = valid_preds.argmax(axis=-1)
+	# stop_words = text2.ENGLISH_STOP_WORDS.union(["http", "https", "amp", "amb"])
+	
+	if is_neural_net:
+		test_preds = test_preds.argmax(axis=-1)
 
-    # if should_do_common:
+	# if should_do_common:
 	   #  feature_names = count_vect.get_feature_names()
 	   #  diff = classifier.feature_log_prob_[1,:] - np.max(classifier.feature_log_prob_[0:])
 
@@ -60,36 +55,36 @@ def train_model(classifier, feature_vector_train, label, feature_vector_valid, i
 	   #     print(names_diff_sorted[i])
 	   #     c += 1
 	   #     i += 1
-    
-    train_acc = metrics.accuracy_score(train_preds, train_y)
-    valid_acc = metrics.accuracy_score(valid_preds, valid_y)
-    cm = metrics.confusion_matrix(valid_y, valid_preds)
-    print('Train Accuracy: ', train_acc)
-    print('Validation Accuracy: ', valid_acc)
-    # print('Confusion matrix: ', cm)
-    return (valid_acc, cm)
+	
+	train_acc = metrics.accuracy_score(train_preds, train_y)
+	test_acc = metrics.accuracy_score(test_preds, test_y)
+	cm = metrics.confusion_matrix(test_y, test_preds)
+	print('Train Accuracy: ', train_acc)
+	print('Test Accuracy: ', test_acc)
+	# print('Confusion matrix: ', cm)
+	return (test_acc, cm)
 
 # load positive labels
 pos = open('data/ira_10000.csv').read()
 npos = 0
 labels, texts = [], []
 for i, line in enumerate(pos.split("\n")):
-    content = line.split(',')
-    if len(content) < 4:
-    	continue;
-    if content[4] != "English":
-    	continue;
-    labels.append(1)
-    texts.append(content[2])
-    npos += 1
+	content = line.split(',')
+	if len(content) < 4:
+		continue;
+	if content[4] != "English":
+		continue;
+	labels.append(1)
+	texts.append(content[2])
+	npos += 1
 
 # load negative labels (random tweets)
 neg = open('data/tweets-2016-10000-textonly.txt').read()
 nneg = 0
 for i, line in enumerate(neg.split("\n")):
-    labels.append(0)
-    texts.append(line)
-    nneg += 1
+	labels.append(0)
+	texts.append(line)
+	nneg += 1
 
 texts, labels = shuffle(texts, labels)
 
@@ -101,14 +96,14 @@ trainDF = pandas.DataFrame()
 trainDF['text'] = texts
 trainDF['label'] = labels
 
-train_x, valid_x, train_y, valid_y = model_selection.train_test_split(trainDF['text'], trainDF['label'])
+train_x, test_x, train_y, test_y = model_selection.train_test_split(trainDF['text'], trainDF['label'], train_size=0.8)
 
 print('Size of training set: ', len(train_x))
-print('Size of validation set:', len(valid_x))
+print('Size of Test set:', len(test_x))
 
 encoder = preprocessing.LabelEncoder()
 train_y = encoder.fit_transform(train_y)
-valid_y = encoder.fit_transform(valid_y)
+test_y = encoder.fit_transform(test_y)
 
 binary_count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}', binary=True)
 binary_count_vect.fit(trainDF['text'])
@@ -117,51 +112,28 @@ count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
 count_vect.fit(trainDF['text'])
 
 binary_xtrain_count = binary_count_vect.transform(train_x)
-binary_xvalid_count = binary_count_vect.transform(valid_x)
+binary_xtest_count = binary_count_vect.transform(test_x)
 
 xtrain_count =  count_vect.transform(train_x)
-xvalid_count =  count_vect.transform(valid_x)
+xtest_count =  count_vect.transform(test_x)
 
 # word level tf-idf
 tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=5000)
 tfidf_vect.fit(trainDF['text'])
 xtrain_tfidf =  tfidf_vect.transform(train_x)
-xvalid_tfidf =  tfidf_vect.transform(valid_x)
+xtest_tfidf =  tfidf_vect.transform(test_x)
 
 # ngram level tf-idf 
 tfidf_vect_ngram = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
 tfidf_vect_ngram.fit(trainDF['text'])
 xtrain_tfidf_ngram =  tfidf_vect_ngram.transform(train_x)
-xvalid_tfidf_ngram =  tfidf_vect_ngram.transform(valid_x)
+xtest_tfidf_ngram =  tfidf_vect_ngram.transform(test_x)
 
 # characters level tf-idf
 tfidf_vect_ngram_chars = TfidfVectorizer(analyzer='char', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
 tfidf_vect_ngram_chars.fit(trainDF['text'])
 xtrain_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(train_x) 
-xvalid_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(valid_x) 
-
-if deep_nn:
-	# load the pre-trained word-embedding vectors 
-	embeddings_index = {}
-	for i, line in enumerate(open('data/wiki-news-300d-1M.vec')):
-	    values = line.split()
-	    embeddings_index[values[0]] = np.asarray(values[1:], dtype='float32')
-
-	# create a tokenizer 
-	token = text.Tokenizer(num_words=5000)
-	token.fit_on_texts(trainDF['text'])
-	word_index = token.word_index
-
-	# convert text to sequence of tokens and pad them to ensure equal length vectors 
-	train_seq_x = sequence.pad_sequences(token.texts_to_sequences(train_x), maxlen=70)
-	valid_seq_x = sequence.pad_sequences(token.texts_to_sequences(valid_x), maxlen=70)
-
-	# create token-embedding mapping
-	embedding_matrix = np.zeros((len(word_index) + 1, 300))
-	for word, i in word_index.items():
-	    embedding_vector = embeddings_index.get(word)
-	    if embedding_vector is not None:
-	        embedding_matrix[i] = embedding_vector
+xtest_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(test_x) 
 
 # API function for server to predict using nb (using count vectors)
 nb_classifier = naive_bayes.MultinomialNB()
@@ -183,123 +155,100 @@ if nb:
 
 	# Naive Bayes on Count Vectors
 	print("NB, Binary Count Vectors: ")
-	accuracy = train_model(naive_bayes.MultinomialNB(), binary_xtrain_count, train_y, binary_xvalid_count)
+	accuracy = train_model(naive_bayes.MultinomialNB(), binary_xtrain_count, train_y, binary_xtest_count)
 
 	# Naive Bayes on Count Vectors
 	print("NB, Count Vectors: ")
-	accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_count, train_y, xvalid_count)
+	accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_count, train_y, xtest_count)
 
 	# Naive Bayes on Word Level TF IDF Vectors
 	print("NB, WordLevel TF-IDF: ")
-	accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf, train_y, xvalid_tfidf)
+	accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf, train_y, xtest_tfidf)
 
 	# Naive Bayes on Ngram Level TF IDF Vectors
 	print("NB, N-Gram Vectors: ")
-	accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
+	accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf_ngram, train_y, xtest_tfidf_ngram)
 
 	# Naive Bayes on Character Level TF IDF Vectors
 	print("NB, CharLevel Vectors: ")
-	accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf_ngram_chars, train_y, xvalid_tfidf_ngram_chars)
+	accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf_ngram_chars, train_y, xtest_tfidf_ngram_chars)
 
 if lr:
+
+	# LR on Count Vectors
+	print("LR, Binary Count Vectors: ")
+	accuracy = train_model(linear_model.LogisticRegression(), binary_xtrain_count, train_y, binary_xtest_count)
+
+
 	# Linear Classifier on Count Vectors
 	print("LR, Count Vectors: ")
-	accuracy = train_model(linear_model.LogisticRegression(), xtrain_count, train_y, xvalid_count)
+	accuracy = train_model(linear_model.LogisticRegression(), xtrain_count, train_y, xtest_count)
 
 	# Linear Classifier on Word Level TF IDF Vectors
 	print("LR, WordLevel TF-IDF: ")
-	accuracy = train_model(linear_model.LogisticRegression(), xtrain_tfidf, train_y, xvalid_tfidf)
+	accuracy = train_model(linear_model.LogisticRegression(), xtrain_tfidf, train_y, xtest_tfidf)
 
 	# Linear Classifier on Ngram Level TF IDF Vectors
 	print("LR, N-Gram Vectors: ")
-	accuracy = train_model(linear_model.LogisticRegression(), xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
+	accuracy = train_model(linear_model.LogisticRegression(), xtrain_tfidf_ngram, train_y, xtest_tfidf_ngram)
 
 	# Linear Classifier on Character Level TF IDF Vectors
 	print("LR, CharLevel Vectors: ")
-	accuracy = train_model(linear_model.LogisticRegression(), xtrain_tfidf_ngram_chars, train_y, xvalid_tfidf_ngram_chars)
+	accuracy = train_model(linear_model.LogisticRegression(), xtrain_tfidf_ngram_chars, train_y, xtest_tfidf_ngram_chars)
 
-# if svm_:
+if svm_:
 
+	def svm_tune(x, y):
+		Cs = [0.001, 0.01, 0.1, 1, 10]
+		gammas = [0.001, 0.01, 0.1, 1]
+		grid = {'C': Cs, 'gamma': gammas}
+		search = GridSearchCV(svm.SVC(kernel='rbf'), grid)
+		search.fit(x, y)
+		search.best_params_
+		return search.best_params_
+
+	# print(svm_tune(xtrain_count, train_y))
+	# print(svm_tune(xtrain_tfidf, train_y))
+	# print(svm_tune(xtrain_tfidf_ngram, train_y))
+
+	# # SVM on Bin Count Vectors
+	# print("SVM, Binary Count Vectors: ")
+	# accuracy = train_model(svm.SVC(kernel='rbf', C=10, gamma=0.1), binary_xtrain_count, train_y, binary_xtest_count)
 
 	# # SVM on Count Vectors
 	# print("SVM, Count Vectors: ")
-	# accuracy = train_model(svm.SVC(), xtrain_count, train_y, xvalid_count)
+	# accuracy = train_model(svm.SVC(kernel='rbf', C=10, gamma=0.1), xtrain_count, train_y, xtest_count)
 
 	# # SVM on Word Level TF IDF Vectors
 	# print("SVM, WordLevel TF-IDF: ")
-	# accuracy = train_model(svm.SVC(), xtrain_tfidf, train_y, xvalid_tfidf)
+	# accuracy = train_model(svm.SVC(kernel='rbf', C=10, gamma=0.1), xtrain_tfidf, train_y, xtest_tfidf)
 
 	# # SVM on Ngram Level TF IDF Vectors
 	# print("SVM, N-Gram Vectors: ")
-	# accuracy = train_model(svm.SVC(), xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
+	# accuracy = train_model(svm.SVC(kernel='rbf', C=10, gamma=0.1), xtrain_tfidf_ngram, train_y, xtest_tfidf_ngram)
 
-	# # SVM on Character Level TF IDF Vectors
-	# print("SVM, CharLevel Vectors: ")
-	# accuracy = train_model(svm.SVC(), xtrain_tfidf_ngram_chars, train_y, xvalid_tfidf_ngram_chars)
+	# SVM on Character Level TF IDF Vectors
+	print("SVM, CharLevel Vectors: ")
+	accuracy = train_model(svm.SVC(kernel=linear), xtrain_tfidf_ngram_chars, train_y, xtest_tfidf_ngram_chars)
 
 if rf:
+
+	# SVM on Bin Count Vectors
+	print("RF, Binary Count Vectors: ")
+	accuracy = train_model(ensemble.RandomForestClassifier(), binary_xtrain_count, train_y, binary_xtest_count)
+
 	# RF on Count Vectors
-	print("SVM, Count Vectors: ")
-	accuracy = train_model(ensemble.RandomForestClassifier(), xtrain_count, train_y, xvalid_count)
+	print("RF, Count Vectors: ")
+	accuracy = train_model(ensemble.RandomForestClassifier(), xtrain_count, train_y, xtest_count)
 
 	# RF on Word Level TF IDF Vectors
-	print("SVM, WordLevel TF-IDF: ")
-	accuracy = train_model(ensemble.RandomForestClassifier(), xtrain_tfidf, train_y, xvalid_tfidf)
+	print("RF, WordLevel TF-IDF: ")
+	accuracy = train_model(ensemble.RandomForestClassifier(), xtrain_tfidf, train_y, xtest_tfidf)
 
 	# RF on Ngram Level TF IDF Vectors
-	print("SVM, N-Gram Vectors: ")
-	accuracy = train_model(ensemble.RandomForestClassifier(), xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
+	print("RF, N-Gram Vectors: ")
+	accuracy = train_model(ensemble.RandomForestClassifier(), xtrain_tfidf_ngram, train_y, xtest_tfidf_ngram)
 
 	# RF on Character Level TF IDF Vectors
-	print("SVM, CharLevel Vectors: ")
-	accuracy = train_model(ensemble.RandomForestClassifier(), xtrain_tfidf_ngram_chars, train_y, xvalid_tfidf_ngram_chars)
-
-# if shallow_nn:
-# 	def create_model_architecture(input_size):
-# 	    # create input layer 
-# 	    input_layer = layers.Input((input_size, ), sparse=True)
-	    
-# 	    # create hidden layer
-# 	    hidden_layer = layers.Dense(400, activation="relu")(input_layer)
-	    
-# 	    # create output layer
-# 	    output_layer = layers.Dense(1, activation="sigmoid")(hidden_layer)
-
-# 	    classifier = models.Model(inputs = input_layer, outputs = output_layer)
-# 	    classifier.compile(optimizer=optimizers.Adam(), loss='binary_crossentropy')
-# 	    return classifier 
-
-# 	classifier = create_model_architecture(xtrain_tfidf_ngram.shape[1])
-# 	print("NN, Ngram Level TF IDF Vectors")
-# 	accuracy = train_model(classifier, xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram, is_neural_net=True)
-
-# if deep_nn:
-
-# 	def create_cnn():
-# 	    # Add an Input Layer
-# 	    input_layer = layers.Input((70, ))
-
-# 	    # Add the word embedding Layer
-# 	    embedding_layer = layers.Embedding(len(word_index) + 1, 300, weights=[embedding_matrix], trainable=False)(input_layer)
-# 	    embedding_layer = layers.SpatialDropout1D(0.3)(embedding_layer)
-
-# 	    # Add the convolutional Layer
-# 	    conv_layer = layers.Convolution1D(100, 3, activation="relu")(embedding_layer)
-
-# 	    # Add the pooling Layer
-# 	    pooling_layer = layers.GlobalMaxPool1D()(conv_layer)
-
-# 	    # Add the output Layers
-# 	    output_layer1 = layers.Dense(50, activation="relu")(pooling_layer)
-# 	    output_layer1 = layers.Dropout(0.25)(output_layer1)
-# 	    output_layer2 = layers.Dense(1, activation="sigmoid")(output_layer1)
-
-# 	    # Compile the model
-# 	    model = models.Model(inputs=input_layer, outputs=output_layer2)
-# 	    model.compile(optimizer=optimizers.Adam(), loss='binary_crossentropy')
-	    
-# 	    return model
-
-# 	classifier = create_cnn()
-# 	print("CNN, Word Embeddings")
-# 	accuracy = train_model(classifier, train_seq_x, train_y, valid_seq_x, is_neural_net=True)
+	print("RF, CharLevel Vectors: ")
+	accuracy = train_model(ensemble.RandomForestClassifier(), xtrain_tfidf_ngram_chars, train_y, xtest_tfidf_ngram_chars)
--- server.py
+++ server.py
@@ -1,7 +1,17 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
 from run_all import nb_predict
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from flask import jsonify
+import threading
+
+#import OAuth secrets from secret.py
+import secrets
 
 print('Classifier trained.')
+
+last_tweet = None
 
 app = Flask(__name__)
 
@@ -16,5 +26,50 @@ def index():
 	else:
 		return render_template('template.html')
 
+@app.route("/live", methods=['GET'])
+def live():
+	return render_template('live.html')
+
+@app.route("/livedata", methods=['GET'])
+def livedata():
+	global last_tweet
+	pred, prob = nb_predict(last_tweet.text)
+	return jsonify({
+		'prediction': int(pred[0]),
+		'probability': float(prob[0][1]),
+		'text': last_tweet.text,
+		'author': last_tweet.author.screen_name
+		})
+
+
+class StdOutListener(StreamListener):
+	""" A listener handles tweets that are received from the stream.
+	This is a basic listener that just prints received tweets to stdout.
+	"""
+	def on_status(self, status):
+		global last_tweet
+		if(status.text.startswith('RT')):
+			return
+		print(status.text)
+		last_tweet = status
+		return True
+
+	def on_error(self, status_code):
+		if status_code == 420:
+			return False
+
+def ingest():
+
+	l = StdOutListener()
+	auth = OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
+	auth.set_access_token(secrets.access_token, secrets.access_token_secret)
+
+	stream = Stream(auth, l)
+	stream.filter(track=['trump'])
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+
+	thread = threading.Thread(target = ingest)
+	thread.start()
+
+	app.run(host='0.0.0.0', port=80)
